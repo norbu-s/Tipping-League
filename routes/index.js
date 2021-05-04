68d3ec9f0ebed7983569747481c497aa6c514ad0
Norbu Sherpa <norbuts@hotmail.com>
2021-05-04 22:53:57 +1000

    Updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,11 @@
-module.exports = {
-    findAll: function(req, res) {
-      db.Tipping_League
-        .find(req.query)
-        .sort({ date: -1 })
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    findById: function(req, res) {
-      db.Tipping_League
-        .findById(req.params.id)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    create: function(req, res) {
-      db.Tipping_League
-        .create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    update: function(req, res) {
-      db.Tipping_League
-        .findOneAndUpdate({ _id: req.params.id }, req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    remove: function(req, res) {
-      db.Tipping_League
-        .findById({ _id: req.params.id })
-        .then(dbModel => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    }
-  };
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
