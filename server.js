32fe364a8bf4cb56819eb69375515d8f0a3199f6
Norbu Sherpa <norbuts@hotmail.com>
2021-05-04 20:06:10 +1000

    updated folder structure and packgae.json

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-  
-const axios = require('axios');
-const express = require('express');
-
-// Sets up the Express App
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-// const db = require('./models');
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-// Static directory
-// app.use(express.static(''));
-
-// Routes
-// require('')(app);
-// require('')(app);
-
-// Syncing our sequelize models and then starting our Express app
-
-
-// db.sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`));
-// });
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
