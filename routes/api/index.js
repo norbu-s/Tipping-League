c68c81799bfae79bbf377a08be00c1dd9ce3a55c
Norbu Sherpa <norbuts@hotmail.com>
2021-05-04 23:10:28 +1000

    Added Passport files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,6 @@
 const router = require("express").Router();
-const leagueController = require("../../controllers/competitionController");
-const tipsController = require("../../controllers/tipsController");
+const userRoutes = require("./user");
 
-router.route("/")
-  .get(leagueController.getAll)
-  .post(leagueController.save);
-
-router.route("/:id")
-  .delete(leagueController.delete);
-
-router.route("/tips")
-  .get(leagueController.getAll)
-  .post(leagueController.save);
+router.use("/user", userRoutes);
 
 module.exports = router;
