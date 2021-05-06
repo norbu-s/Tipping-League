e56a117502798a897ef646fb65dd730dbf965fc2
Norbu Sherpa <norbuts@hotmail.com>
2021-05-06 22:11:02 +1000

    updated schema

--- db/schemal.sql
+++ db/schemal.sql
@@ -11,12 +11,14 @@ team_name VARCHAR(255) NOT NULL,
 PRIMARY KEY (id)
 );
 
-CREATE TABLE User(
+
+CREATE TABLE Users(
 id INT AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(20) NOT NULL,
 last_name VARCHAR(20) NOT NULL,
-username VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
 password VARCHAR(255) NOT NULL,
+notification Boolean,
 PRIMARY KEY (id)
 );
 
@@ -26,7 +28,7 @@ id INT AUTO_INCREMENT NOT NULL,
 competition_name VARCHAR(255) NOT NULL,
 userId INT NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (userId) references User (id)
+FOREIGN KEY (userId) references Users (id)
 );
 
 
@@ -36,8 +38,19 @@ competitionId INT NOT NULL,
 userId INT NOT NULL,
 teamId INT NOT NULL,
 game INT NOT NULL,
-FOREIGN KEY (userId) references User (id),
+FOREIGN KEY (userId) references Users (id),
 FOREIGN KEY (teamId) references Teams (id),
 FOREIGN KEY (competitionId) references Competition (id),
 PRIMARY KEY (id)
 );
+
+USE Tipping_League;
+
+CREATE TABLE Results(
+id INT AUTO_INCREMENT NOT NULL,
+points INT NOT NULL,
+tipID INT NOT NULL,
+Round INT NOT NULL,
+FOREIGN KEY (tipID) references Tips (id),
+PRIMARY KEY (id)
+)
