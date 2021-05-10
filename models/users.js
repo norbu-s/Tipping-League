0e2de6cd169304b3ea948ab4955714a90bf3a569
Norbu Sherpa <norbuts@hotmail.com>
2021-05-10 21:26:56 +1000

    Fixed connection route

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/config.json');
 
 class User extends Model {
   checkPassword(loginPw) {
