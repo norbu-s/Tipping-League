16dc2722d6c550844aafa1cc0cf096b986cb25de
Norbu Sherpa <norbuts@hotmail.com>
2021-05-18 19:24:48 +1000

    deleting query folder

--- mysqlQueries/draw.js
+++
@@ -1,8 +0,0 @@
-const draw = {
-    function() {
-
-"Select id, usersId, name, team_name, match_number, draw from Tipping_League.Teams Inner Join Tipping_League.Tips using(id)Inner Join Tipping_League.Users using (id)Inner Join Tipping_League.Fixtures using (id) where draw != '0' and fixturesId = '12';"
-    }
-};
-
-module.exports = draw;
--- mysqlQueries/win.js
+++
@@ -1,7 +0,0 @@
-const draw = {
-    function() {
-    "Select id, teamsId, tipsId, team_name, match_number, draw from Tipping_League.Teams Inner Join Tipping_League.Tips using(id)Inner Join Tipping_League.Fixtures using (id) where draw = '0' and match_number = '9';"
-    }
-};
-
-module.exports = draw;
