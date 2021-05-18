b9f786329f8f27b4b56bae275ae4099d94426069
Norbu Sherpa <norbuts@hotmail.com>
2021-05-18 22:33:22 +1000

    heroku fix

--- models/index.js
+++ models/index.js
@@ -14,7 +14,11 @@ if (config.use_env_variable) {
     config.database,
     config.username,
     config.password,
-    { dialect: config.dialect }
+    {
+      dialect: config.dialect,
+      port: config.port,
+      host: config.host,
+    }
   );
 }
 
